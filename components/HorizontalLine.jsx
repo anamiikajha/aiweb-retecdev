2e618d7cef3fbb2164f7909de531245d12a8124a
Anamika Jha <ajha5169@gmail.com>
2024-07-11 18:58:58 +0530

    feat: btn bg change on focus tab done

--- components/HorizontalLine.jsx
+++ components/HorizontalLine.jsx
@@ -1,36 +1,32 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 
 const HorizontalLine = () => {
   const [scrollPosition, setScrollPosition] = useState(0);
+  const [activeIndex, setActiveIndex] = useState(0);
+  const tagRefs = useRef([]);
 
   useEffect(() => {
     const handleScroll = () => {
-      // current scroll position
       const position = window.scrollY;
-      // Update the state with the new scroll position
       setScrollPosition(position);
+      console.log(position)
     };
 
-    // Add event listener when scrolling
     window.addEventListener("scroll", handleScroll);
-
     return () => {
       window.removeEventListener("scroll", handleScroll);
     };
   }, []);
+
   return (
-    <>
-      <div
-        className="transition-block-box"
-        style={{ overflow: "hidden", width: "98.5%" }}
-      >
-        <a
+    <div className="transition-block-box" style={{ overflow: "hidden", width: "98.5%" }}>
+         <a
           href="#"
           className="box-tag box-inline-block"
           style={{
             willChange: "background",
-            backgroundColor: "rgb(14, 18, 23)",
-            color: "rgb(255, 255, 255)",
+            backgroundColor: `${(scrollPosition<=242)?"rgb(255, 255, 255)":"rgb(14, 18, 23)"}`,
+            color: `${(scrollPosition<=242)?"rgb(14, 18, 23)":"rgb(255, 255, 255)"}`,
           }}
         >
           <div className="tag-block-text">Build</div>
@@ -40,8 +36,8 @@ const HorizontalLine = () => {
           className="box-tag box-inline-block"
           style={{
             willChange: "background",
-            backgroundColor: "rgb(14, 18, 23)",
-            color: "rgb(255, 255, 255)",
+            backgroundColor: `${(scrollPosition<=486 && scrollPosition>=242.39999389648438)?"rgb(255, 255, 255)":"rgb(14, 18, 23)"}`,
+            color: `${(scrollPosition<=486 && scrollPosition>=242.39999389648438)?"rgb(14, 18, 23)":"rgb(255, 255, 255)"}`,
           }}
         >
           <div className="tag-block-text">Test</div>
@@ -50,9 +46,9 @@ const HorizontalLine = () => {
           href="#"
           className="box-tag box-inline-block"
           style={{
-            color: "rgb(255, 255, 255)",
             willChange: "background",
-            backgroundColor: "rgb(14, 18, 23)",
+            backgroundColor: `${(scrollPosition<=730 && scrollPosition>=486.3999938964844)?"rgb(255, 255, 255)":"rgb(14, 18, 23)"}`,
+            color: `${(scrollPosition<=730 && scrollPosition>=486.3999938964844)?"rgb(14, 18, 23)":"rgb(255, 255, 255)"}`,
           }}
         >
           <div className="tag-block-text">Deploy</div>
@@ -61,25 +57,23 @@ const HorizontalLine = () => {
           href="#"
           className="box-tag box-inline-block"
           style={{
-            color: "rgb(255, 255, 255)",
             willChange: "background",
-            backgroundColor: "rgb(14, 18, 23)",
+            backgroundColor: `${(scrollPosition>=754)?"rgb(255, 255, 255)":"rgb(14, 18, 23)"}`,
+            color: `${(scrollPosition>=754)?"rgb(14, 18, 23)":"rgb(255, 255, 255)"}`,
           }}
         >
           <div className="tag-block-text">Monitor</div>
         </a>
-        <div
-          className="trickle"
-          style={{
-            willChange: "width, height",
-            width: `${scrollPosition * 0.12}%`,
-            transition: "width 0.2s ease-out", // Smooth transition
-          }}
-        />
-        <div className="trickle-bg" />
-      </div>
-      <div className="box-block" />
-    </>
+      <div
+        className="trickle"
+        style={{
+          willChange: "width, height",
+          width: `${scrollPosition * 0.12}%`,
+          transition: "width 0.2s ease-out",
+        }}
+      />
+      <div className="trickle-bg" />
+    </div>
   );
 };
 
